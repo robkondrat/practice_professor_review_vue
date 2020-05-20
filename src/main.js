456a4d3eee87c8ae142d72620a84ea929fb1916b
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-05-20 11:27:45 -0500

    initialized vue frontend

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
 
 new Vue({
+  el: '#app',
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
